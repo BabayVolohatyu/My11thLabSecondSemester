b090924684da44f92363a1268aa010f13e6b5ed3
BabayVolohatyu <babayvolohatyu@gmail.com>
2024-05-13 13:51:52 +0300

    Have done fourth task try#2

--- Lab/main.cpp
+++ Lab/main.cpp
@@ -2,14 +2,14 @@
 #include <thread>
 #include <mutex>
 
-void task3(){
+void task3() {
     int counter = 0;
     std::mutex my_mutex;
     std::thread::id id;
-    auto function = [&]   {
+    auto function = [&] {
         my_mutex.lock();
         id = std::this_thread::get_id();
-        counter+=*reinterpret_cast<int*>(&id);
+        counter += *reinterpret_cast<int *>(&id);
         std::cout << counter << std::endl;
         my_mutex.unlock();
     };
@@ -20,30 +20,34 @@ void task3(){
     t2.join();
     t3.join();
 }
-void task4(){
+
+void task4() {
     std::mutex mutex1;
     std::mutex mutex2;
     std::thread::id id;
-    auto f1 = [&]{
+    auto f1 = [&] {
         std::lock_guard<std::mutex> lock1(mutex1);
         std::lock_guard<std::mutex> lock2(mutex2);
         id = std::this_thread::get_id();
-        std::cout << "Thread #"<<id << " is running" << std::endl;
+        std::cout << "Thread #" << id << " is running" << std::endl;
     };
-    auto f2 = [&]{
-        std::lock_guard<std::mutex> lock2(mutex2);
+    auto f2 = [&] {
         std::lock_guard<std::mutex> lock1(mutex1);
+        std::lock_guard<std::mutex> lock2(mutex2);
         id = std::this_thread::get_id();
-        std::cout << "Thread #"<<id << " is running" << std::endl;
+        std::cout << "Thread #" << id << " is running" << std::endl;
+
     };
     std::thread t1(f1);
     std::thread t2(f2);
     t1.join();
     t2.join();
 }
-void task5(){
-    
+
+void task5() {
+
 }
+
 int main() {
     //task3();
     task4();
